748375cb9217fe77c02785ef40ef7f026cb687a5
Huntez <osigor5464@gmail.com>
2022-11-10 23:20:54 +0200

    fully aplha build of social network

--- 010_PyMYSQL/fourth.py
+++ 010_PyMYSQL/fourth.py
@@ -15,22 +15,23 @@ try:
                 password = input("Enter a password : ")
             except Exception as error:
                 print(error)
-            cursor.execute(f"select login from Logins where username = '{username}'")
+            cursor.execute(f"select password from Logins where username = '{username}'")
             password_check = cursor.fetchall()
             try:
-                if password_check[0].get('login') == password:
+                if password_check[0]['password'] == password:
                     print("Okay!"); break
                 else:
                     print("Bad password!")
             except:
                 print("Incorrect user")
         while True:
-            enter = int(input("1 - list, 2 - add, 3 - del, 4 - all del, 5 - replace : "))
+            enter = int(input("1 - list, 2 - add, 3 - del, 4 - all del, 5 - replace, 6 - show : "))
             if enter == 1:
-                cursor.execute("select * from koshikDB.koshik")
+                cursor.execute("select * from users")
                 result = cursor.fetchall()
                 for i in result:
-                    print(i.get('id'), ":", i.get('product'))             
+                    for j in i:
+                        print(j, ":", i[j])
             elif enter == 2:
                 name = input("Name : ")
                 surname = input("Surname : ")
@@ -41,34 +42,47 @@ try:
                 values ('{name}', '{surname}', '{info}', '{friends}', '{publications}')''')
                 connection.commit()
             elif enter == 3:
-                cursor.execute("select * from koshikDB.koshik")
+                cursor.execute("select * from users")
                 result = cursor.fetchall()
                 for i in result:
-                    print(i.get('id'), ":", i.get('product'))
-                uenter = int(input("Enter a product id : "))
-                cursor.execute(f"delete from koshikDB.koshik where id = {uenter}")
+                    print(i['id'], "-", i['name'], i['surname'])
+                uenter = int(input("Enter a user id : "))
+                cursor.execute(f"delete from users where id = {uenter}")
                 connection.commit()
             elif enter == 4:
-                cursor.execute(f"delete from koshikDB.koshik")
+                cursor.execute(f"delete from users")
                 connection.commit()
+                print("Okay!")
             elif enter == 5:
-                cursor.execute("select * from koshikDB.koshik")
+                id = input("Enter a user id : ")
+                cursor.execute(f"select * from users where id = '{id}'")
                 result = cursor.fetchall()
                 for i in result:
-                    print(i.get('id'), ":", i.get('product'))
-                replace_id = input("Enter id of product : ")
-                replace = input("Enter for replace : ")
-                cursor.execute(f"update koshik set product = '{replace}' where id = '{replace_id}'")
+                     print(i['id'], "-", i['name'], i['surname'])
+                uenter = int(input("1 - name, 2 - surname, 3 - info : "))
+                if uenter == 1:
+                    name = input("Name : ")
+                    cursor.execute(f"update users set name = '{name}' where id = '{id}'")
+                elif uenter == 2:
+                    surname = input("Surname : ")
+                    cursor.execute(f"update users set surname = '{surname}' where id = '{id}'")
+                elif uenter == 3:
+                    info = input("Info : ")
+                    cursor.execute(f"update users set info = '{info}' where id = '{id}'")                   
                 connection.commit()
             elif enter == 6:
-                select = int(input("1, 2, 3 : ")) # Select action from another two tasks  
-                if select == 1:
-                    find_product = input("Enter a product name : ")
-                    cursor.execute(f"select * from koshik where product = '{find_product}'")
-                elif select == 2:
-                    cursor.execute(f"select * from koshik where id = (select max(id) from koshik)")
+                id = input("Enter a user id : ")
+                cursor.execute(f"select * from users where id = '{id}'")
                 result = cursor.fetchall()
-                for i in result:
-                  print(i.get('id'), ":", i.get('product'))
+                uenter = int(input("1 - info, 2 - friends, 3 - publications, 4 - all : "))
+                if uenter == 1:
+                    print("Info : ", result[0]['info'])
+                elif uenter == 2:
+                    print("Friends : ", result[0]['friends'])
+                elif uenter == 3:
+                    print("Publications : ", result[0]['publications'])
+                elif uenter == 4:
+                    for i in result[0]:
+                        print(i, ":", result[0][i])
 except Exception as error:
     print(error)
