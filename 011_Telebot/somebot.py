e831aa2fbc3a5638d371f65ed0411082432fbcce
Huntez <osigor5464@gmail.com>
2022-11-10 09:53:34 +0200

    functional from word counter to one bot

---
+++ 011_Telebot/somebot.py
@@ -0,0 +1,42 @@
+import telebot
+import connections
+
+bot = telebot.TeleBot(connections.token)
+@bot.message_handler(commands=['calc', 'happy_ticket'])
+
+def get_text(message):
+    if message.text == "/calc":
+        somevar = bot.send_message(message.chat.id, "Enter a action : ")
+        bot.register_next_step_handler(somevar, calc)
+    elif message.text == "/happy_ticket":
+        somevar = bot.send_message(message.chat.id, "Enter a ticket : ")
+        bot.register_next_step_handler(somevar, happy_ticket)
+
+def count_words(message):
+    count = 0
+    for i in message.text.split(" "):
+        count += 1
+    bot.send_message(message.chat.id, "word count : " + str(count))    
+
+def happy_ticket(message):
+    ticket = message.text
+    if sum(ticket[1:len(ticket)//2]) == sum(ticket[(len(ticket)//2 + 1):]):
+        bot.send_message(message.chat.id, "Ticket - " + ticket + " is happy!")
+    else:
+        bot.send_message(message.chat.id, "Ticket - " + ticket + " is unhappy..")
+
+def calc(message):
+    alist = ["+", "-", "*", "/"]
+    svar = [i for i in message.text if i in alist]
+    ab = [i for i in message.text.replace(" ", "").split(svar[0])]
+    a, b = int(ab[0]), int(ab[1])
+    if svar[0] == "+":
+        bot.send_message(message.chat.id, str(a)+" + "+str(b)+" = "+str(a + b))
+    elif svar[0] == "-":
+        bot.send_message(message.chat.id, str(a)+" - "+str(b)+" = "+str(a - b))
+    elif svar[0] == "*":
+        bot.send_message(message.chat.id, str(a)+" * "+str(b)+" = "+str(a * b))
+    elif svar[0] == "/":
+        bot.send_message(message.chat.id, str(a)+" / "+str(b)+" = "+str(a / b))
+
+bot.infinity_polling()
