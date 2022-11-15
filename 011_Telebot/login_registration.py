734f3d478bb5eb992311d8ea94bef21be0c576c3
Huntez <osigor5464@gmail.com>
2022-11-15 21:53:55 +0200

    adding connection.close to login_registration

--- 011_Telebot/login_registration.py
+++ 011_Telebot/login_registration.py
@@ -86,6 +86,7 @@ class check_authorization:
             print(error)
         finally:
             self.connection.commit()
+            self.connection.close()
     
     def authorization_check(self):
         try:
@@ -104,5 +105,6 @@ class check_authorization:
             print(error)
         finally:
             self.connection.commit()
+            self.connection.close()
     
     
